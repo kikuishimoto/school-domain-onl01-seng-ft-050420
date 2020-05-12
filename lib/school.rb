aa1d847247d0fd4bbc5c5c7eda9d5bf977c058a6
kiku ishimoto <kikuish@gmail.com>
2020-05-12 15:42:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  attr_reader :name, :
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+
+  def add_student=(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+
+    roster[student_name] = grade
+end
